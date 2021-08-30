9ab1b7cf8fa535aeb50eb67437221b9938b7e2b6
ecka5 <69878365+ecka5@users.noreply.github.com>
2021-08-30 11:58:27 -0500

    mio

--- PROYECTO/settings.py
+++ PROYECTO/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'PROYECTO.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':'bdproyecto',
+        'USER':'postgres',
+        'PASSWORD':'1234',
+        'HOST':'127.0.0.1',
+        'DATABASE_PORT':'5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
