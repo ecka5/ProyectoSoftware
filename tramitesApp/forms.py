18d320b4111228966588186c9c8d933ba24ba435
RobertoValdivia <rjvaldivia212000@gmail.com>
2021-08-31 00:33:52 -0500

    agregue las otras partes en el forms.py

--- tramitesApp/forms.py
+++ tramitesApp/forms.py
@@ -1,8 +1,18 @@
 from django import forms
 from django.forms import fields
-from .models import Alumno
+from .models import Alumno,TipoTramite,Tramite
 
 class AlumnoForm(forms.ModelForm): 
     class Meta:
         model=Alumno
+        fields='__all__'
+
+class TipoTramiteForm(forms.ModelForm): 
+    class Meta:
+        model=TipoTramite
+        fields='__all__'
+
+class TramiteForm(forms.ModelForm): 
+    class Meta:
+        model=Tramite
         fields='__all__'
