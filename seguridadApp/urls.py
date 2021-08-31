b365451aa088916c6c383aab712b023384238c86
ecka5 <69878365+ecka5@users.noreply.github.com>
2021-08-30 23:58:39 -0500

    Revert "Merge branch 'main' of https://github.com/ecka5/ProyectoSoftware into main"
    This reverts commit 5cfcdd1efd796fef89338f0c53a63e28e709cf4b, reversing
    changes made to dd9a4d14ccd82466acf5d9ba28fffd0893b5d18b.

--- seguridadApp/urls.py
+++ seguridadApp/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
-from seguridadApp.views import acceder, homePage
-from django.contrib.auth import views
+from django.urls import path
+# from seguridadApp.views import acceder,homePage,salir
 
-urlpatterns = [
-    path('', acceder, name = "login"),
-    path('home/', homePage, name="home"),
+# urlpatterns = [
+#     path('', acceder, name="login"),
+#     path('home/', homePage, name="home"),
 #     path('logout/',salir,name="logout"),
-]
+# ]
